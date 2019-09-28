b114342ff6146e001bb2bd652eee0e7bb5d0a4d8
sami suliman <sami.bco@gmail.com>
2019-09-28 14:32:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,10 +37,10 @@ attr_accessor :id, :name, :grade
   if self.id
     self.update
   else
-  sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?,?)
-  SQL
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+    SQL
 
    DB[:conn].execute(sql, self.name, self.grade) 
    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
